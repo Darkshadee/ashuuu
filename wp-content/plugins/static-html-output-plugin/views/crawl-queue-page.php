b16e174273f44114e7db6b461f3123feed9e9013
Darkshadee <maashwin115@gmail.com>
2021-03-06 15:15:51 +0530

    ch

---
+++ wp-content/plugins/static-html-output-plugin/views/crawl-queue-page.php
@@ -0,0 +1,15 @@
+<br>
+
+<table class="widefat striped">
+    <thead>
+        <tr>
+            <th>URLs in Crawl Queue</th>
+        </tr>
+    </thead>
+    <tbody>
+                    <tr>
+                <td>Crawl queue is empty.</td>
+            </tr>
+        
+            </tbody>
+</table>
