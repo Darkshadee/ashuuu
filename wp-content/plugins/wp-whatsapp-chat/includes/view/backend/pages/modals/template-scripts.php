aad8ac6d43686b8a42ffc5709894bb04597b4a0c
Darkshadee <maashwin115@gmail.com>
2021-03-07 17:04:02 +0530

    chk

---
+++ wp-content/plugins/wp-whatsapp-chat/includes/view/backend/pages/modals/template-scripts.php
@@ -0,0 +1,31 @@
+<script type="text/html" id="tmpl-qlwapp-modal-window">
+ 
+</script>
+
+<script type="text/html" id="tmpl-subview-header"> 
+   
+</script>
+
+<script type="text/html" id="tmpl-subview-tabs">
+ 
+</script>
+
+<script type="text/html" id="tmpl-subview-contact">
+ 
+</script>
+
+<script type="text/html" id="tmpl-subview-contact-chat">
+ 
+</script>
+
+<script type="text/html" id="tmpl-subview-contact-info"> 
+  
+</script> 
+
+<script type="text/html" id="tmpl-subview-visibility">
+ 
+</script>
+
+<script type="text/html" id="tmpl-subview-footer">
+     
+</script>
